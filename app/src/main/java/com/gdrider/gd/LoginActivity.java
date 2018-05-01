859c8e3cf4037b05b5837c8c982fe3fbc75b6418
Kuanlin-Chen <webb1992810625@gmail.com>
2018-05-01 22:51:11 +0800

    1)Implement facebook login

--- app/src/main/java/com/gdrider/gd/LoginActivity.java
+++ app/src/main/java/com/gdrider/gd/LoginActivity.java
@@ -1,34 +1,71 @@
 package com.gdrider.gd;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
-public class LoginActivity extends AppCompatActivity implements LoginContext.ILoginView{
+public class LoginActivity extends AppCompatActivity {
 
     private Button button_guest_login;
     private LoginButton button_fb_login;
+    private CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         button_guest_login = (Button)findViewById(R.id.button_guest_login);
         button_fb_login = (LoginButton)findViewById(R.id.button_fb_login);
+
+        callbackManager = CallbackManager.Factory.create();
+        LoginManager.getInstance().registerCallback(callbackManager,
+                new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+                        // App code
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        // App code
+                    }
+
+                    @Override
+                    public void onError(FacebookException exception) {
+                        // App code
+                    }
+                });
+
+        // Callback registration
+        button_fb_login.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                // App code
+            }
+
+            @Override
+            public void onCancel() {
+                // App code
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                // App code
+            }
+        });
     }
 
-    // Override LoginContext.View method
     @Override
-    public void onLoginSuccess(String message) {
-        //this.textView_result.setText(message);
-    }
-
-    @Override
-    public void onLoginFail(String message) {
-        //this.textView_result.setText(message);
-        //this.editText_password.setText("");
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
