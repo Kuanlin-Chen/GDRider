9ee740f4d5d2157554d0026799dea5b04e6c9969
Kuanlin-Chen <webb1992810625@gmail.com>
2018-05-08 19:37:46 +0800

    1)Remove unnecessary code

--- app/src/main/java/com/gdrider/gd/LoginActivity.java
+++ app/src/main/java/com/gdrider/gd/LoginActivity.java
@@ -1,14 +1,12 @@
 package com.gdrider.gd;
 
 import android.content.Intent;
-import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
-import android.widget.Toast;
 
 import com.facebook.AccessToken;
 import com.facebook.login.widget.LoginButton;
