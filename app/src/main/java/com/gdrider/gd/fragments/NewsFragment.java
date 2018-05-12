59d8d529d8bfb505019147196c2e78bf61d3490c
Kuanlin-Chen <webb1992810625@gmail.com>
2018-05-12 17:29:33 +0800

    1)Add the dependencies of RecylerCiew and CardView 2)Implement RecyclerView and CardView in NewsFragment 3)Add the demo picture

--- app/src/main/java/com/gdrider/gd/fragments/NewsFragment.java
+++ app/src/main/java/com/gdrider/gd/fragments/NewsFragment.java
@@ -2,11 +2,17 @@ package com.gdrider.gd.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.gdrider.gd.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class NewsFragment extends Fragment {
 
@@ -26,6 +32,55 @@ public class NewsFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         System.out.println(label+"onCreateView");
-        return inflater.inflate(R.layout.fragment_news, container, false);
+        //return inflater.inflate(R.layout.fragment_news, container, false);
+        View view = inflater.inflate(R.layout.fragment_news, container, false);
+
+        ArrayList<String> myDataset = new ArrayList<>();
+        for(int i = 0; i < 50; i++){
+            myDataset.add(i + " SOL 迷彩 消光黑");
+        }
+        RecyclerAdapter myAdapter = new RecyclerAdapter(myDataset);
+        RecyclerView recyclerView = (RecyclerView)view.findViewById(R.id.recycler_view);
+
+        GridLayoutManager mLayoutManager = new GridLayoutManager(getActivity(),2);
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setAdapter(myAdapter);
+
+        return view;
+    }
+
+    public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHolder> {
+
+        private List<String> mData;
+        public class ViewHolder extends RecyclerView.ViewHolder {
+            public TextView mTextView;
+            public ViewHolder(View v) {
+                super(v);
+                mTextView = (TextView) v.findViewById(R.id.info_text);
+            }
+        }
+
+        public RecyclerAdapter(List<String> data) {
+            mData = data;
+        }
+
+        @Override
+        public RecyclerAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View v = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.item, parent, false);
+            ViewHolder vh = new ViewHolder(v);
+            return vh;
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+            holder.mTextView.setText(mData.get(position));
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return mData.size();
+        }
     }
 }
