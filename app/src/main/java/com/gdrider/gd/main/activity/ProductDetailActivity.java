40e8cb363769d8b57b456eec72724c6d087bf677
Kuanlin-Chen <webb1992810625@gmail.com>
2018-06-12 22:45:12 +0800

    Implement new Activity for product detail
    Cause:
    Make this page more clear
    
    Solution:
    Add ProductDetailAdapter.java and item_detail.xml
    Modify ProductDetailActivity.java and activity_product_detail.xml
    Use RecyclerView to show product detail

--- app/src/main/java/com/gdrider/gd/main/activity/ProductDetailActivity.java
+++ app/src/main/java/com/gdrider/gd/main/activity/ProductDetailActivity.java
@@ -2,36 +2,67 @@ package com.gdrider.gd.main.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.ImageSwitcher;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.ViewSwitcher;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.Toast;
 
 import com.gdrider.gd.R;
+import com.gdrider.gd.main.contract.Contract;
+import com.gdrider.gd.main.custom.ProductDetailAdapter;
+import com.gdrider.gd.main.presenter.Presenter;
 
-public class ProductDetailActivity extends AppCompatActivity{
+import java.util.ArrayList;
 
-    private ImageSwitcher imageSwitcher;
-    private TextView textView;
+public class ProductDetailActivity extends AppCompatActivity implements Contract.View{
+
+    private Contract.Presenter presenter;
+    private ProductDetailAdapter productDetailAdapter;
+    private RecyclerView recyclerView;
+
+    //Just for testing
+    private int[] mImage = {
+            R.drawable.gdemo,
+            R.drawable.gdemo,
+            R.drawable.gdemo
+    };
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_detail);
 
-        imageSwitcher = (ImageSwitcher)findViewById(R.id.image_switcher);
-        imageSwitcher.setFactory(new ViewSwitcher.ViewFactory() {
-            @Override
-            public View makeView() {
-                ImageView imageView = new ImageView(ProductDetailActivity.this);
-                imageView.setAdjustViewBounds(true);
-                return imageView;
-            }});
-        imageSwitcher.setImageResource(R.drawable.gdemo);
+        recyclerView = (RecyclerView)findViewById(R.id.recycler_view);
 
-        textView = (TextView)findViewById(R.id.info_text);
-        textView.setText("Test");
+        //get data via presenter
+        presenter = new Presenter(this);
+
+        productDetailAdapter = new ProductDetailAdapter("SOL SF3 全罩安全帽", "黑", 2700, mImage, this, presenter);
+        GridLayoutManager mLayoutManager = new GridLayoutManager(this,1);
+        recyclerView.setAdapter(productDetailAdapter);
+        recyclerView.setLayoutManager(mLayoutManager);
+    }
+
+    @Override
+    public void setNews(ArrayList<String> title, ArrayList<String> color, ArrayList<Integer> price, ArrayList<Integer> image){
 
     }
 
+    @Override
+    public void setEven(ArrayList<String> title, ArrayList<String> color, ArrayList<Integer> price, ArrayList<Integer> image){
+
+    }
+
+    @Override
+    public void setCart(ArrayList<String> title, ArrayList<String> color, ArrayList<Integer> price, ArrayList<Integer> image){
+
+    }
+
+    @Override
+    public void setFavo(ArrayList<String> title, ArrayList<String> color, ArrayList<Integer> price, ArrayList<Integer> image){
+
+    }
+
+    @Override
+    public void setToast(String string){
+        Toast.makeText(this, string, Toast.LENGTH_SHORT).show();
+    }
 }
---
+++ app/src/main/java/com/gdrider/gd/main/custom/ProductDetailAdapter.java
@@ -0,0 +1,143 @@
+package com.gdrider.gd.main.custom;
+
+import android.content.Context;
+import android.os.Handler;
+import android.os.Looper;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageButton;
+import android.widget.ImageSwitcher;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.ViewSwitcher;
+
+import com.gdrider.gd.R;
+import com.gdrider.gd.main.contract.Contract;
+
+public class ProductDetailAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
+
+    private final int TYPE_FIRST_ITEM = 0;
+    private final int TYPE_ITEM = 1;
+    private String mTitle;
+    private String mColor;
+    private Integer mPrice;
+    private int[] mImage;
+    private Context context;
+    private Handler imageSwitcherHandler;
+    private int animationCounter = 0;
+    private Contract.Presenter presenter;
+
+    public ProductDetailAdapter(String title, String color, Integer price, int[] image, Context context, Contract.Presenter presenter){
+        this.mTitle = title;
+        this.mColor = color;
+        this.mPrice = price;
+        this.mImage = image;
+        this.context = context;
+        this.presenter = presenter;
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        switch (viewType) {
+            case TYPE_FIRST_ITEM:
+                final View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_ads, parent, false);
+                return new ImageViewHolder(view);
+            case TYPE_ITEM:
+                final View view2 = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_detail, parent, false);
+                return new DetailViewHolder(view2);
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, final int position) {
+        switch (holder.getItemViewType()) {
+            case TYPE_FIRST_ITEM:
+                final ImageViewHolder imageViewHolder = (ImageViewHolder) holder;
+                imageViewHolder.imageSwitcher.setInAnimation(AnimationUtils.loadAnimation(context, android.R.anim.slide_in_left));
+                imageViewHolder.imageSwitcher.setOutAnimation(AnimationUtils.loadAnimation(context, android.R.anim.slide_out_right));
+                imageViewHolder.imageSwitcher.setFactory(new ViewSwitcher.ViewFactory() {
+                    @Override
+                    public View makeView() {
+                        ImageView imageView = new ImageView(context);
+                        imageView.setAdjustViewBounds(true);
+                        return imageView;
+                    }});
+                imageViewHolder.imageSwitcher.setImageResource(mImage[0]);
+                //Update image every five second
+                imageSwitcherHandler = new Handler(Looper.getMainLooper());
+                imageSwitcherHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        switch (animationCounter) {
+                            case 0:
+                                imageViewHolder.imageSwitcher.setImageResource(mImage[animationCounter]);
+                                break;
+                            case 1:
+                                imageViewHolder.imageSwitcher.setImageResource(mImage[animationCounter]);
+                                break;
+                            case 2:
+                                imageViewHolder.imageSwitcher.setImageResource(mImage[animationCounter]);
+                                break;
+                        }
+                        if(++animationCounter > 2 ) animationCounter = 0;
+                        imageSwitcherHandler.postDelayed(this, 5000);
+                    }
+                });
+                break;
+            case TYPE_ITEM:
+                DetailViewHolder detailViewHolder = (DetailViewHolder) holder;
+                detailViewHolder.textView_title.setText(mTitle+" "+mColor);
+                detailViewHolder.textView_price.setText("$"+String.valueOf(mPrice));
+                detailViewHolder.imageButton_favo.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        presenter.showToast("Add to favorite!");
+                    }
+                });
+                break;
+        }
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (position == 0) {
+            return TYPE_FIRST_ITEM;
+        } else {
+            return TYPE_ITEM;
+        }
+    }
+
+    final class ImageViewHolder extends RecyclerView.ViewHolder {
+
+        ImageSwitcher imageSwitcher;
+
+        public ImageViewHolder(View itemView) {
+            super(itemView);
+            imageSwitcher = (ImageSwitcher)itemView.findViewById(R.id.image_switcher);
+        }
+    }
+
+    final class DetailViewHolder extends RecyclerView.ViewHolder {
+
+        TextView textView_title;
+        TextView textView_price;
+        ImageButton imageButton_favo;
+
+        public DetailViewHolder(View itemView) {
+            super(itemView);
+            textView_title = (TextView)itemView.findViewById(R.id.info_text);
+            textView_price = (TextView)itemView.findViewById(R.id.info_price);
+            imageButton_favo = (ImageButton)itemView.findViewById(R.id.info_favo);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return 2;
+    }
+}
